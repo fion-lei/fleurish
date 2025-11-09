5af3cbac3959b2e29f9b5a6513111a3f791e21bf
LychellePham <lychellepham731@gmail.com>
2025-11-08 20:59:47 -0700

    create page for request board

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,19 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import { Link } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
-  return [{ title: "New React Router App" }, { name: "description", content: "Welcome to React Router!" }];
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <>
+      <Welcome />
+      <Link to="/request-board">Request Board</Link>
+    </>
+  );
 }
